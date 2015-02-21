b3351a85e73bee47c788d6518ed3f2177d9f5f4d
cpiggott <cpiggott@ksu.edu>
2015-02-21 14:49:30 -0600

    Added Parse to the project

--- app/src/main/java/com/pathogenstudios/dogr/dogr/MainActivity.java
+++ app/src/main/java/com/pathogenstudios/dogr/dogr/MainActivity.java
@@ -18,6 +18,8 @@ import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import com.parse.Parse;
+
 
 public class MainActivity extends ActionBarActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
@@ -45,6 +47,8 @@ public class MainActivity extends ActionBarActivity
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
+
+        
     }
 
     @Override
